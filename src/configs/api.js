d1e89d3824ab3e682b92b1a46d1c0256e872a6bd
femiliapm <femilia.putri@ui.ac.id>
2022-06-24 05:23:03 +0700

    renaming image, add axios, add landing page service

---
+++ src/configs/api.js
@@ -0,0 +1,14 @@
+import { default as axios } from "axios";
+
+const fetchAPI = async ({ url, method, data, headers }) => {
+  const responseAxios = await axios({
+    url,
+    method,
+    data,
+    headers,
+  }).catch((err) => err.response);
+
+  return responseAxios;
+};
+
+export default fetchAPI;
---
+++ src/services/landing.service.js
@@ -0,0 +1,9 @@
+import fetchAPI from "../configs/api";
+
+const ROOT_API = process.env.REACT_APP_API_LECTRONIC;
+
+export const getLandingPageAPI = async () => {
+  const url = `${ROOT_API}/product?landing=true`;
+  const response = await fetchAPI({ url, method: "get" });
+  return response;
+};
